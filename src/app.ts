38b0d4480efc61a3c7245ed1cb112f0783306f29
Pedro Samuel <pedrosviki1265@gmail.com>
2024-08-27 17:59:49 -0400

    feat: Add router for handling routes

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express from "express";
+import router from "./routes/routes";
 
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use("/", router);
 
 export default app;
