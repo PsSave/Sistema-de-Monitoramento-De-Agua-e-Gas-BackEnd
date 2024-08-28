7aae7d37b4718872b1b0c73ea2355ec34695062f
root <root@DESKTOP-D995UT3>
2024-08-28 14:07:46 -0400

    feat: Add query insert to dabase values and alter function and params name to data

--- src/services/UploadService.ts
+++ src/services/UploadService.ts
@@ -1,14 +1,15 @@
-import { MeasureRequest } from "../types/meansure";
+import { MeasureRequest } from "../types/measure";
 import { genAI } from "../utils/genAI";
 import { v4 as uuidv4 } from "uuid";
+import { query } from "../database/db";
 
 class UploadService {
-  async uploadFile(file: MeasureRequest) {
+  async uploadData(data: MeasureRequest) {
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
     function fileToGenerativePart() {
       return {
         inlineData: {
-          data: file.image,
+          data: data.image,
           mimeType: "image/jpeg",
         },
       };
@@ -18,11 +19,22 @@ class UploadService {
     const imagePart = fileToGenerativePart();
 
     const result = await model.generateContent([prompt, imagePart]);
-    const imageUrl = `data:image/jpeg;base64,${file.image}`;
+    const imageUrl = `data:image/jpeg;base64,${data.image}`;
+    const measureUuid = uuidv4();
+
+    await query('INSERT INTO measurements_pending (customer_code, measure_datetime, measure_type, measure_value, image_url, measure_uuid) VALUES ($1, $2, $3, $4, $5, $6)', [
+      data.customer_code,
+      data.measure_datetime,
+      data.measure_type,
+      result.response.text(),
+      imageUrl,
+      measureUuid
+    ])
+    
     return {
       image_url: imageUrl,
-      measure_value: result.response.text(),
-      measure_uuid: uuidv4(),
+      measure_value: parseInt(result.response.text()), // Converte o valor para inteiro
+      measure_uuid: measureUuid
     };
   }
 }
