5478b8b3e40e160cef301702f1e00583fe69aa4d
root <root@DESKTOP-D995UT3>
2024-08-28 17:55:11 -0400

    feat: Add Confirm Controller in routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import UploadController from "../controllers/UploadController";
+import ConfirmController from "../controllers/ConfirmController";
 
 
 const router = Router();
 const uploadController = new UploadController();
+const confirmController = new ConfirmController();
 
 router.post("/upload", uploadController.uploadData);
+router.patch("/confirm", confirmController.confirmedData);
 
 export default router;
