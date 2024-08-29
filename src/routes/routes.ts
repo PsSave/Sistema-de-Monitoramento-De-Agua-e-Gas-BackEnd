929f1412e5ec466a2d0a3edc81e01ab30d2837e2
root <root@DESKTOP-D995UT3>
2024-08-28 22:57:20 -0400

    feat: Add get route to list customer and type filter

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,13 +1,16 @@
 import { Router } from "express";
 import UploadController from "../controllers/UploadController";
 import ConfirmController from "../controllers/ConfirmController";
+import ListController from "../controllers/ListController";
 
 
 const router = Router();
 const uploadController = new UploadController();
 const confirmController = new ConfirmController();
+const listController = new ListController();
 
 router.post("/upload", uploadController.uploadData);
 router.patch("/confirm", confirmController.confirmedData);
+router.get("/:customer_code/list", listController.getList);
 
 export default router;
